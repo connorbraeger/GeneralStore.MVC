6622d09ef638f288f5390d7a354f4653e3633ca7
Connor Braeger <cjbraeger@gmail.com>
2021-03-25 20:56:41 -0400

    Added Delete Functionality

--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -15,12 +15,13 @@ namespace GeneralStore.MVC.Controllers
         {
             return View(_db.Products.ToList());
         }
-        // Get: Product
+        // Get: Create
         public ActionResult Create()
         {
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(Product product)
         {
             if (ModelState.IsValid)
@@ -30,6 +31,30 @@ namespace GeneralStore.MVC.Controllers
                 return RedirectToAction("Index");
             }
             return View(product);
+        }
+        // Get: Delete
+        // Product/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _db.Products.Find(id);
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
