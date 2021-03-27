5db87f310861b4f9e01837e05b0f117cbd73e3fb
Connor Braeger <cjbraeger@gmail.com>
2021-03-26 21:35:42 -0400

    added edit view

--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -41,7 +41,7 @@ namespace GeneralStore.MVC.Controllers
                 return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
             }
             Product product = _db.Products.Find(id);
-            if(product == null)
+            if (product == null)
             {
                 return HttpNotFound();
             }
@@ -55,6 +55,37 @@ namespace GeneralStore.MVC.Controllers
             _db.Products.Remove(product);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+        // Get : Edit
+        // Product/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+
+            }
+            return View(product);
+        }
+
+        // Post : Edit// Product/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(product);
         }
     }
 }
